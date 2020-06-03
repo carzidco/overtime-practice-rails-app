07b2391b5bd38fc0e451ddeaff02073b2866e922
Charlie Cordoba <carzidco@gmail.com>
2020-06-03 08:24:31 -0600

    refactor for posts, adding table for posts, moving post body into a partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
+user = User.create(first_name: 'charlie', last_name: 'cordoba', email: 'carzidco@gmail.com', password: '123456', password_confirmation: '123456')
 100.times do |post|
-  Post.create(date: Date.today, rational: "#{post} rationale content")
+  Post.create(date: Date.today, rational: "#{post} rationale content", user_id: user.id)
 end
 
 puts "100 Posts have been created"
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,30 +1,41 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    @user = User.create(email: 'jsnow3@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Jon', last_name: 'Snow')
+    login_as(@user)
+  end
+
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
       visit posts_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rational: 'Post1', user_id: @user.id)
+      post2 = Post.create(date: Date.today, rational: 'Post2', user_id: @user.id)
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
   describe 'creation' do
-    it 'has a new form can be reached' do
-      user = User.create(email: 'jsnow1@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Jon', last_name: 'Snow')
-      login_as(user)
+    before do
       visit new_post_path
+    end
+    it 'has a new form can be reached' do
       expect(page.status_code).to eq(200)
     end
 
     it 'can be created from new form page' do
-      user = User.create(email: 'jsnow2@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Jon', last_name: 'Snow')
-      login_as(user)
-      visit new_post_path
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rational]', with: 'Some rationale'
       click_on 'Save'
@@ -33,9 +44,6 @@ describe 'navigate' do
     end
 
     it 'will have a user associated it' do
-      user = User.create(email: 'jsnow3@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Jon', last_name: 'Snow')
-      login_as(user)
-      visit new_post_path
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rational]', with: 'User Association'
       click_on 'Save'
